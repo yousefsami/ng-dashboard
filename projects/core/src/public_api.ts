7a4fba57083a0231201adfa2822150f9a625bd79
Ali <connexion.founder@outlook.com>
2019-12-22 00:18:54 +0100

    Complete working from 1.0.5

--- projects/core/src/public_api.ts
+++ projects/core/src/public_api.ts
@@ -5,6 +5,7 @@
 export * from './lib/ng-dashboard.service';
 export * from './lib/ng-dashboard.module';
 export * from './lib/ng5-basic/services/configuration.service';
+export * from './lib/ng5-basic/services/user.service';
 export * from './lib/ng5-basic/components/layout/layout.component';
 export * from './lib/ng5-basic/components/layout-fade/layout-fade.component';
 export * from './lib/ng5-basic/services/helpers';
