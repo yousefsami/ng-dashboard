66931f8c0413ef13e0bc68de786e9865ff22911d
Ali <connexion.founder@outlook.com>
2020-04-03 19:05:12 +0200

    Merge team and profile menu

--- projects/core/src/lib/ng5-basic/components/profile-menu/profile-menu.component.ts
+++ projects/core/src/lib/ng5-basic/components/profile-menu/profile-menu.component.ts
@@ -1,30 +1,65 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
-import { IUser } from '../../definitions';
+import { IUser, Team } from '../../definitions';
 import { RouterService } from '../../services/router.service';
+import { Subscription } from 'rxjs';
+import { ConfigurationService } from '../../services/configuration.service';
 
 @Component({
   selector: 'ng-profile-menu',
   templateUrl: './profile-menu.component.html',
   styleUrls: ['./profile-menu.component.scss']
 })
-export class ProfileMenuComponent {
+export class ProfileMenuComponent implements OnInit, OnDestroy {
+  public teams: Array<Team> = [];
+  public teamsSubscription: Subscription;
+
   public display = '';
-  constructor(public user: UserService, public ngdRouter: RouterService) {}
+  constructor(
+    public user: UserService,
+    public ngdRouter: RouterService,
+    private config: ConfigurationService
+  ) {}
+
+  ngOnInit() {
+    this.teamsSubscription = this.config.Teams.subscribe(
+      teams => (this.teams = teams)
+    );
+  }
+
+  ngOnDestroy() {
+    if (this.teamsSubscription) {
+      this.teamsSubscription.unsubscribe();
+    }
+  }
 
   public name(user: IUser) {
-    if (!user) {
-      return 'GU';
-    }
-    if (user.firstname && user.lastname) {
-      return `${user.firstname.substr(0, 1)}${user.lastname.substr(0, 1)}`;
-    }
     if (user.firstname) {
-      return `${user.firstname.substr(0, 2)}`;
+      return `${user.firstname.substr(0, 10)}`;
     }
     if (user.lastname) {
       return `${user.lastname.substr(0, 2)}`;
     }
     return user.email.substr(0, 2);
+  }
+
+  public get label() {
+    if (!this.teams || this.teams.length === 0) {
+      return null;
+    }
+    const selected = this.teams.find(team => team.$selected);
+
+    if (!selected) {
+      return this.formatLabel(this.teams[0]);
+    }
+    return this.formatLabel(selected);
+  }
+
+  public formatLabel(team: Team) {
+    return team.name;
+  }
+
+  public selectTeam(team: Team) {
+    this.config.SelectedTeam.next(team);
   }
 }
